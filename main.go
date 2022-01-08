3bfeed6e58bb08a4921e9d6b67922056bf17b965
git-shochann <sho_825.sa216@docomo.ne.jp>
2022-01-08 23:52:56 +0900

    ✨: update

--- main.go
+++ main.go
@@ -1,4 +1,64 @@
 package main
 
+import (
+	"goland/mylib"
+	"strconv"
+)
+
+// func main() {
+// 	x := mylib.Input("Type a number!")
+// 	fmt.Print(x + "月は")
+// 	switch n, err := strconv.Atoi(x); n {
+// 	case 0: // p.72
+// 		fmt.Println("整数値が得られません")
+// 		fmt.Println(err)
+// 	case 1, 2, 12:
+// 		fmt.Println("冬です")
+// 	case 3,4,5:
+// 		fmt.Println("春です")
+// 	case 6,7,8:
+// 		fmt.Println("夏です")
+// 	case 9,10,11:
+// 		fmt.Println("秋です")
+// 	default:
+// 		fmt.Println("月の値ではないですよ？")
+// 	}
+// }
+
+// func main() {
+// 	x := mylib.Input("Type a number")
+// 	n, err := strconv.Atoi(x)
+// 	if err == nil {
+c// 	} else {
+// 		return // main関数を終了するという意味
+// 	}
+// 	switch {
+// 		case n%2 == 0:
+// 			fmt.Println("偶数です")
+// 		case n%2 == 1:
+// 			fmt.Println("奇数です")
+// 	}
+// }
+
 func main() {
+	x := mylib.Input("Type a number")
+	n, err := strconv.Atoi(x)
+	if err == nil {
+		fmt.Println("1から" + x + "の偶数の合計は")
+	} else {
+		return
+	}
+	t := 0
+	c := 0
+	for {
+		c++
+		if c%2 == 1 {
+			continue
+		}
+		if c > n {
+			break
+		}
+		t += c
+	}
+	fmt.Println(t, "です")
 }
---
+++ mylib/http.go
@@ -0,0 +1,15 @@
+package mylib
+
+import (
+	"fmt"
+	"net/url"
+)
+
+func Http() {
+	// resp, _ := http.Get("https://www.supremenewyork.com/shop/all")
+	// defer resp.Body.Close()
+	// body, _ := ioutil.ReadAll(resp.Body)
+	// fmt.Println(string(body))
+	base, _ := url.Parse("https://www.supremenewyork.com/shop/all")
+	fmt.Println(base)
+}
