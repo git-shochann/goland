79c2c768fd90a77513dcf9ebfd74d50e874fb3c3
git-shochann <sho_825.sa216@docomo.ne.jp>
2022-01-10 21:19:32 +0900

    ♻️: refactor

--- mylib/http.go
+++ mylib/http.go
@@ -2,14 +2,16 @@ package mylib
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"net/url"
 )
 
 func Http() {
-	// resp, _ := http.Get("https://www.supremenewyork.com/shop/all")
-	// defer resp.Body.Close()
-	// body, _ := ioutil.ReadAll(resp.Body)
-	// fmt.Println(string(body))
+	resp, _ := http.Get("https://www.supremenewyork.com/shop/all")
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(body))
 	base, _ := url.Parse("https://www.supremenewyork.com/shop/all")
 	fmt.Println(base)
 }
