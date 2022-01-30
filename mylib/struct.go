d180266b7cdd35b9d446e2ca119375a6be6634e9
git-shochann <sho_825.sa216@docomo.ne.jp>
2022-01-30 17:44:10 +0900

    🚀: update struct

--- mylib/struct.go
+++ mylib/struct.go
@@ -21,3 +21,27 @@ func init() {
 	// taroという変数は構造体Mydataから初期化している = 関数を所有しているので以下のように呼び出すことが可能
 	taro.PrintData()
 }
+
+// 構造体型の中に構造体型
+
+type User struct {
+	Name string
+	Email string
+	Profile Profile // Profileフィールドを以下のProfile型として設定
+}
+
+type Profile struct {
+	Age int
+}
+
+func Do() {
+
+	user := User{ 
+		Name: "Taro", 
+		Email: "t0825.st@gmail.com",
+		Profile: Profile{Age: 24},
+  }
+  fmt.Printf("%v, %T\n", user, user) // %v -> value, %T -> Type
+  // {Taro t0825.st@gmail.com {24}}, main.User 型はmainのUser型
+
+}
