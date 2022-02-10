8c4f82940f7366f954734af7952dde09b3954bb1
git-shochann <sho_825.sa216@docomo.ne.jp>
2022-02-10 23:58:25 +0900

    🚧: wip func

--- main.go
+++ main.go
@@ -1,6 +1,22 @@
-// 常に実行できるように
-
 package main
 
-func main() {
+// 関数
+// 無名関数
+// 関数を返す関数
+
+// 関数を引数に取る関数
+// func CallFunction(f func()) {
+// 	f() // ここで受け取って実行する
+// }
+
+// func main() {
+// 	CallFunction(func (){
+// 		fmt.Println("testing!")
+// 	})
+// }
+
+// クロージャー
+// 関数を返す関数 引数にstring, 戻り値もstringを返す
+func later() func(string) string {
+	var store string
 }
