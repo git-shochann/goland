0f9e4206e717f2a17d8d806040b6c9cca2eeb9d4
git-shochann <sho_825.sa216@docomo.ne.jp>
2022-02-19 23:59:56 +0900

    ✨: learn logic

---
+++ algebra/algebra.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func add(a int, b int) (result int, state string) {
+	result = a + b
+	state = "a + b"
+	return
+}
+
+func sub(a int, b int) (result int, state string) {
+	result = a - b
+	state = "a - b"
+	return
+}
+
+func mul(a int, b int) (result int, state string) {
+	result = a * b
+	state = "a * b"
+	return
+}
+
+func div(a int, b int) (result int, state string) {
+	result = a / b
+	state = "a / b"
+	return
+}
+
+func main() {
+	oprs := []func(int, int) (int, string){
+		add, sub, mul, div,
+	}
+
+	// fmt.Println(oprs)
+
+	for i := 0; i < len(oprs); i++ {
+		result, state := oprs[i](18, 6) // ここで実行か
+		fmt.Printf("%s = %d \n", state, result)
+	}
+}
