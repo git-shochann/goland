d61eaa31c620310d834c33ec2fcc98b2ac1ea9ff
git-shochann <sho_825.sa216@docomo.ne.jp>
2022-03-25 18:51:14 +0900

    🐞: fix .gitignore

--- learning/asciiart/asciiart.go
+++ learning/asciiart/asciiart.go
@@ -1,30 +1,30 @@
 package main
 
-import (
-	"fmt"
-	"image"
-	"os"
-)
+// import (
+// 	"fmt"
+// 	"image"
+// 	"os"
+// )
 
-func main() {
-	reader, err := os.Open("matrinx.jpg") // return *File -> Read()メソッドを持っている
-	if err != nil {
-		fmt.Println("ファイルの読み取りエラーです", err)
-		os.Exit(1) // プログラム終了
-	}
+// func main() {
+// 	reader, err := os.Open("matrinx.jpg") // return *File -> Read()メソッドを持っている
+// 	if err != nil {
+// 		fmt.Println("ファイルの読み取りエラーです", err)
+// 		os.Exit(1) // プログラム終了
+// 	}
 
-	img, name, err := image.Decode(reader) // 引数にReaderインターフェースを取る -> Readインターフェースを満たしている構造体があればOK -> 戻り値の*File構造体はRead()メソッドを持っている
-	if err != nil {
-		fmt.Println("画像の変換エラーです")
-		os.Exit(1)
-	} else {
-		fmt.Println(name, "形式のデータを取得しました")
-	}
+// 	img, name, err := image.Decode(reader) // 引数にReaderインターフェースを取る -> Readインターフェースを満たしている構造体があればOK -> 戻り値の*File構造体はRead()メソッドを持っている
+// 	if err != nil {
+// 		fmt.Println("画像の変換エラーです")
+// 		os.Exit(1)
+// 	} else {
+// 		fmt.Println(name, "形式のデータを取得しました")
+// 	}
 
-	defer reader.Close() // openしたらcloseする
+// 	defer reader.Close() // openしたらcloseする
 
-	marks := []string("*", "+", "-")
-	var marksStr string
+// 	marks := []string("*", "+", "-")
+// 	var marksStr string
 
-	for y := img.Bounds()
-}
+// 	for y := img.Bounds()
+// }
